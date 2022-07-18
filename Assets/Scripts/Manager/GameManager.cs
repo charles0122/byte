b9534f025cd24f923c2fa04a9ffe2a3d0ef7428e
charles <zainai311@qq.com>
2022-07-18 10:07:07 +0800

    final

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -96,7 +96,7 @@ namespace ByteLoop.Manager
             gameObject.transform.SetParent(GameObject.FindGameObjectWithTag("Env").transform);
             RefalshCurrentProductionDialog();
             RefalshCurrentProductionStationRecipe();
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(1.5f);
             yield return Fade(0);
             
         }
